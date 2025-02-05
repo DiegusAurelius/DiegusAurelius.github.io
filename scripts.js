53009bffae129392b89bc947038203d31c595a7c
DiegusAurelius <diegusaurelius@gmail.com>
2025-02-05 14:58:18 -0600

    Edit scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-console.log('It works!');
+let meaning = 21;
+let life = 21;
+
+function meaningOfLife(num, num) {
+  let what = num + num;
+  console.log(what, ' is the meaning of life!');
+}
+
+meaningOfLife(meaning, life);
