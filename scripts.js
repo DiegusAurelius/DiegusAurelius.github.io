c1ec7ddb5466f89da2ca8b82a150358baaad119b
DiegusAurelius <diegusaurelius@gmail.com>
2025-02-05 15:01:53 -0600

    Add function meaningOfLife

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let life = 21;
 
 function meaningOfLife(num, num) {
   let what = num + num;
-  console.log(what, ' is the meaning of life!');
+  console.log(`${what} is the meaning of life!`);
 }
 
 meaningOfLife(meaning, life);
