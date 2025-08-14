e7125947dc77384fccf55513ffeffb857063cff9
Diegus Aurelius <diegusaurelius@gmail.com>
2025-08-14 16:49:43 -0600

    Upgrade

--- scripts.js
+++
@@ -1,9 +0,0 @@
-let meaning = 21;
-let life = 21;
-
-function meaningOfLife(num, num) {
-  let what = num + num;
-  console.log(`${what} is the meaning of life!`);
-}
-
-meaningOfLife(meaning, life);
